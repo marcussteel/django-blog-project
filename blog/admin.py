0e20d66259ef61b0137a0a350559268dac98a620
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 18:49:23 +0300

    models admin added

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category,Post
+from .models import Category,Post,Like,PostView,Comment
 
 admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(PostView)
+admin.site.register(Comment)
