4d8cb0ec641e509fd7436bd7a487d3c6b91aecdf
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 13:25:46 +0300

    model is added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
+from email import contentmanager
+from email.mime import image
+from multiprocessing import AuthenticationError
+from turtle import title
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    OPTIONS = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT )
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True, unique=True)
+
+
+
+    # title
+    # content
+    # image
+    # category
+    # publishdate
+    # lastupdated
+    # Author 
+    # status draft published
+    # slug field  örn django how-to-learn-django
