480ca2ca2f4290b515daa45b484b54eae8ba658b
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 18:41:31 +0300

    all models are done

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
+from distutils.command.upload import upload
 from email import contentmanager
 from email.mime import image
+from email.policy import default
+from importlib.resources import contents
 from multiprocessing import AuthenticationError
 from tabnanny import verbose
 from turtle import title
@@ -8,17 +11,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+#media yüklendiğinde media nın içinde blog oluşturaacak 0 yerine id si ile , 1 yeride ise dosya ismi ile bir klasöt oluşturup içine atacak
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}.format(instance.author.id, filename)'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
-
 # Burası admin panelde Categorys görünmesi yerine Categories olarak  görünmesi için
     class Meta:
         verbose_name_plural = 'Categories'
-
 # Burası admin panelde veya ilk çağırılan yerde default hangi başlığın görüeceğini tespit eder
     def __str__(self):
         return self.name
+
+
 
 class Post(models.Model):
     OPTIONS = (
@@ -27,7 +35,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
     category = models.ForeignKey(Category, on_delete=models.PROTECT )
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
@@ -39,13 +47,28 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    time_stamp =  models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
 
-    # title
-    # content
-    # image
-    # category
-    # publishdate
-    # lastupdated
-    # Author 
-    # status draft published
-    # slug field  örn django how-to-learn-django
+    def __str__(self):
+        return self.user.username
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ blog/signals.py
@@ -0,0 +1,26 @@
+import imp
+
+#oluşturtuğum postu kaydetmeden önce bana slug oluştursun isteyeceğim
+from django.db.models.signals import pre_save
+
+# postu save  et dediğim zaman benim yazdığım kodun gerçekleşmesni sağlıyor, önce bekle ben şu işlemi yapacağım sonra kaydetme işlemini tamamla
+from django.dispatch import receiver
+
+# slug fieldların arasına tire koyan fomksiyon boşluklara tire koyuyor 
+from django.template.defaultfilters import slugify
+
+
+from .models import Post
+
+#decorator iki parametre alıyor. 
+
+
+#instance posttan aldığımız veriyi düşünün, **kwargs ise sayısını bilmediğimiz argümanlar getirebilir python çaışırken
+@receiver(pre_save, sender=Post)
+def pre_save_create_slug(sender,instance, **kwargs):
+    #slug yoksa oluşturacak ve boşluklara tire koyacak
+    if not instance.slug:
+        instance.slug = slugify(instance.author.username + " " + instance.title)
+
+a= "mjs ce e"
+print(slugify(a))
