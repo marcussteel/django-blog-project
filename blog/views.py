c7c4c5ce06921f713b13def23b18cc4780735146
marcussteel <resetyourcoin21@gmail.com>
2022-09-14 09:59:17 +0300

    ceate_post is added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
-from django.shortcuts import render
-
+from tkinter import N
+from django.shortcuts import render, redirect
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -12,3 +13,21 @@ def post_list(request):
         "object_list":qs
     }
     return render(request, "blog/post_list.html", context)
+
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+    # form = PostForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            #şimdi user kimse o author olacak. form objesini kaydet ama database e gönderme diyoruz
+            post = form.save(commit=False)
+            print(request.user)
+            post.author = request.user
+            post.save()
+            # urls'e  app_name = "blog"  ekleyerek hangi appteki list olduğunu belirlemiş olduk
+            return redirect("blog:list")
+    context = {
+        'form':form
+    }
+    return render(request, "blog/post_create.html",context)
