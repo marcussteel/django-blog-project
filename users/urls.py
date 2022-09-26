69a29743956bd238f9b69cb0295bcaae25bcbf65
marcussteel <resetyourcoin21@gmail.com>
2022-09-26 10:53:54 +0300

    multiple name space and app name is not working

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,20 @@
+
 import imp
 from importlib import import_module
+from re import template
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register
+from .views import register,profile
 
-
+app_name = "users"
 
 urlpatterns = [
     # biz django.contrib.auth import views as auth_views i komple aldık, bunun içinden istediğimizi alacağız
     # name : login olmak zorunda gereklilik
-    path('login', auth_views.LoginView.as_view , name='login'),
-    path('logout', auth_views.LogoutView.as_view , name='logout'),
-    path('register/', register , name='register'),
+    path('login', auth_views.LoginView.as_view (template_name="users/login.html"), name='login'),
+    path('logout', auth_views.LogoutView.as_view(
+        template_name="users/logout.html"), name='logout'),
+    path('register/', register, name='register' ),
+    path('profile/', profile, name='profile'),
 
 ]
